222db4fac37b42d9ffaaeeb59a72968c0e81405b
misaelmoc <misael.moctezuma@upr.edu>
2022-11-10 20:13:06 -0400

    Add new user basic route w/ handler & dao

--- main.py
+++ main.py
@@ -1,14 +1,39 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, g
 from flask_cors import CORS, cross_origin
+from dotenv import load_dotenv
+from handler.accounts import Accounts
+
+
+load_dotenv(".env")
 
 #Activate
 app = Flask(__name__)
 #Apply CORS to this app
 CORS(app)
 
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
+
+
 @app.route('/')
 def greeting():
     return 'Hello Im Batman'
+
+
+@app.route('/email/accounts', methods=['POST'])
+def handleAccount():
+    if request.method == 'POST':
+
+        return Accounts().addNewAccount(request.json)
+    else:
+        return jsonify("Method not allowed"), 405
+
+
 
 if __name__ == '__main__':
     app.run()
