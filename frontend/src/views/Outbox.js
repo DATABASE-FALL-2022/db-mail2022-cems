22f288f92b5f465febaa122ccf4bc35a7e16328e
César Amaro <36521013+cesaramaro@users.noreply.github.com>
2022-12-12 01:49:52 -0400

    Fixed outbox not showing when empty

--- frontend/src/views/Outbox.js
+++ frontend/src/views/Outbox.js
@@ -4,7 +4,14 @@ import { ListGroup, Card } from 'react-bootstrap';
 
 export default function Outbox() {
 	const [data, setData] = useState([]);
-	const emailList = data.map((value) => <Email key={value.m_id} info={value} page={'outbox'} />);
+	var emailList = {};
+
+	try {
+		emailList = data.map((value) => <Email key={value.m_id} info={value} page={'outbox'} />);
+	} catch (error) {
+		console.log(error);
+		emailList = <p className='p-5 fw-bold'>Your outbox is empty...</p>;
+	}
 
 	useEffect(() => {
 		var user = JSON.parse(localStorage.getItem('user'));
