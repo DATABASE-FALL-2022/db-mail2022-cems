7c1013d89e374dc41516c07fb179ffb0f8aeb79d
Sebastian Maldonado <sg.maldonador01@gmail.com>
2022-12-11 21:48:14 -0400

    Implement option to delete friends in Friend view

--- frontend/src/views/Friends.js
+++ frontend/src/views/Friends.js
@@ -1,48 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { Card } from 'react-bootstrap';
 import * as Icon from 'react-bootstrap-icons';
+import FriendCard from '../components/FriendCard';
 
 export default function Friends() {
-
-	function formatPhone(phoneNum) {
-		// Phone format received: 0123456789.
-		
-		// Phone formated to be returned:
-		// (012)345-6789.
-		var formatted = "(";
-		formatted += phoneNum.substring(0, 3);
-		formatted += ")";
-		formatted += phoneNum.substring(3, 6);
-		formatted += "-";
-		formatted += phoneNum.substring(6, 10);
-		return formatted;
-	}
 
 	const [user, setUser] = useState([]);
 	var loggedUserID = JSON.parse(localStorage.getItem('user')).user_id;
 	
 	const friendList = user.map((value) => 
-		<div>
-			<Card className='text-center'>
-				<Card.Body>
-					<Card.Title className='row gap-4'>
-						<Icon.PersonCircle size='5rem' />
-						<div className='fw-bold'>
-							{value.first_name} {value.last_name}
-						</div>
-					</Card.Title>
-					<Card.Subtitle>{value.email_address}</Card.Subtitle>
-					<Card.Text className='col mt-2'>
-						<div>{value.is_premium}</div>
-						<div>{formatPhone(value.phone_number)}</div>	
-						<br />
-					</Card.Text>
-				</Card.Body>
-				<Card.Footer className='text-muted'>
-				</Card.Footer>
-			</Card>
-			<br />
-		</div>
+		<FriendCard data={value}/>
 	);
 
 	useEffect(() => {
